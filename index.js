28f380a3ad4621dd527c2da250aae5620bc23875
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-08 15:38:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+// function saturdayFun(){
+//     return `This saturday, I want to roller-skate!`
+// }
+
+// function saturdayFun(){
+//     return "This Saturday, I want to roller-skate!"
+//     // console.log("This Saturday, I want to roller-skate!")
+// }
+
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(job = "go to the office"){
+    return `This Monday, I will ${job}.`
+}
+// let result = "*";
+// let emphatic = "a hard worker";
+let  wrapAdjective = function(result= "*"){
+    return function (emphatic="a hard worker"){
+        return `You are ${result}${emphatic}${result}!`
+    }
+    
+}
